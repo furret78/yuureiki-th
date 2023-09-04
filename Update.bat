7bf63eb74d280257dbf8b54cbcde1907eafc826d
Furret78 <reimuhakuhaku@gmail.com>
2023-09-04 21:41:59 +0700

    create first submit

---
+++ Update.bat
@@ -0,0 +1,2 @@
+C:\Users\reimu\AppData\Local\Programs\Python\Python311\python.exe C:\Users\reimu\Documents\Documents\Games\thcrap\bin\scripts\repo_update.py
+pause
---
+++ repo.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [ "nmlgc/base_tsa", "nmlgc/script_latin", "nmlgc/western_name_order" ]
+	"contact": "reimuhakuhaku@gmail.com",
+	"id": "Yuureiki",
+	"patches": {},
+	"servers": [
+		"https://raw.githubusercontent.com/furret78/yuureiki-th/master/"
+	],
+	"title": "yuureiki's Patches"
+}
