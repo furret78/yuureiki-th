1996ff44e394dcdcf23ccfd9d806045c6f02c62b
Furret78 <68061718+furret78@users.noreply.github.com>
2023-09-04 22:10:31 +0700

    Update repo.js

--- repo.js
+++ repo.js
@@ -6,4 +6,8 @@
 		"https://raw.githubusercontent.com/furret78/yuureiki-th/master/"
 	],
 	"title": "yuureiki's Patches"
+	"patches": {
+		"th18border": "HBM border misalignment fix.",
+		"th19pofv": "PoFV appearance for UDoALG game board."
+	}
 }
