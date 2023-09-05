5389241bbcae271c1b9811a97d35f0a26e61ce01
Furret78 <reimuhakuhaku@gmail.com>
2023-09-05 14:19:33 +0700

    new patch

---
+++ th19oldspr/patch.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "th19oldspr",
+  "servers": [
+    "https://mirrors.thpatch.net/yuureiki/th19oldspr/"
+  ],
+  "title": "Changes certain boss sprites to their non-UDoALG counterparts."
+}
