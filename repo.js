17c72b28d2b50eeb48f90f3a8f1f0504c1dc40c8
Furret78 <reimuhakuhaku@gmail.com>
2023-11-12 16:42:56 +0700

    new patch

--- repo.js
+++ repo.js
@@ -9,6 +9,7 @@
 		"th185border": "HBM border misalignment fix.",
 		"th19pofvui": "PoFV appearance for UDoALG game board.",
 		"th19oldspr": "Swaps certain boss sprites for their non-UDoALG counterparts.",
-		"th19gohei": "Restores player Reimu's purification rod."
+		"th19gohei": "Restores player Reimu's purification rod.",
+		"th19nazrincard": "Swaps Nazrin's incorrectly displayed Ability Card art."
 	}
 }
---
+++ th19nazrincard/patch.js
@@ -0,0 +1,10 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa"
+  ],
+  "id": "th19nazrincard",
+  "servers": [
+    "https://mirrors.thpatch.net/yuureiki/th19nazrincard/"
+  ],
+  "title": "Swaps Nazrin's incorrectly displayed Ability Card art."
+}
