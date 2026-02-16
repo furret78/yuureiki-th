3bd6360f80edbd9d60b9a3b37174c7fa3c5bef8d
Furret78 <68061718+furret78@users.noreply.github.com>
2026-02-16 19:56:41 +0100

    Update patch.js

--- th19nazrincard/patch.js
+++ th19nazrincard/patch.js
@@ -6,5 +6,5 @@
   "servers": [
     "https://mirrors.thpatch.net/yuureiki/th19nazrincard/"
   ],
-  "title": "Swaps Nazrin's incorrectly displayed Ability Card art."
+  "title": "Swaps Nazrin's incorrectly displayed Ability Card art in v1.00. This was fixed in v1.10 of the game."
 }
